399dd2523569265e69f552cbfed423c996662037
Collin Poczatek <collin.poczatek@gmail.com>
2022-01-06 11:53:49 -0800

    simplify providers, vars

--- providers.tf
+++ providers.tf
@@ -1,16 +1,8 @@
 provider "oci" {
-  fingerprint          = var.fingerprint
-  private_key_path     = var.private_key_path
   region               = var.region
-  tenancy_ocid         = var.tenancy_ocid
-  user_ocid            = var.user_ocid
 }
 
 provider "oci" {
-  fingerprint      = var.fingerprint
-  private_key_path = var.private_key_path
   region           = lookup(data.oci_identity_regions.home-region.regions[0], "name")
-  tenancy_ocid     = var.tenancy_ocid
-  user_ocid        = var.user_ocid
   alias            = "home"
 }
--- variables.tf
+++ variables.tf
@@ -10,22 +10,8 @@ variable "compartment_ocid" {
 variable "user_ocid" {
 }
 
-variable "fingerprint" {
-}
-
-variable "private_key_path" {
-}
-
 variable "region" {
   # List of regions: https://docs.cloud.oracle.com/iaas/Content/General/Concepts/regions.htm#ServiceAvailabilityAcrossRegions
   description = "The oci region where resources will be created."
   type        = string
-}
-
-variable "ssh_public_key" {
-  description = "SSH Public Key"
-}
-
-variable "ssh_private_key" {
-  description = "SSH Public Key"
 }
